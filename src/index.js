30634d8b10865d2aebc2c21734aef1e1c82f7721
Daniel Gantz <daniel.gantz@gmail.com>
2020-05-29 18:01:52 -0400

    add src folder with placeholders

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './index.css';
+
+const App = () => {
+  return (
+    <div>
+        <h1>No Shuffle is Better</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
