1ad9f8c8380900cc7831c6f7530368299c135b75
Daniel Gantz <daniel.gantz@gmail.com>
2020-09-25 12:37:49 -0400

    start to making a post

--- src/components/PlaylistSelectForm.js
+++ src/components/PlaylistSelectForm.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
 
-class PlaylistSelectForm extends Component {
-    
-    
-    
-    render() {
-        return (
-            <div>
-                <h2>Select your playlist</h2>
-                
-            </div>
+const PlaylistSelectForm = () => {
+
+    return (
+        <div>
+            <h2>Select your playlist</h2>
             
-        );
-    }
+        </div>
+        
+    );
 }
 
 export default PlaylistSelectForm;
--- src/pages/PostNew.js
+++ src/pages/PostNew.js
@@ -1,21 +1,15 @@
 import React, { Component } from 'react';
 import PlaylistSelectForm from 'Components/PlaylistSelectForm';
 
-class PostNew extends Component {
-    
-    componentDidMount() {
+const PostNew = () => {
+
+    return (
+        <div>
+            <h1>Here is the new post page</h1>
+            <PlaylistSelectForm />
+        </div>
         
-    }
-    
-    render() {
-        return (
-            <div>
-                <h1>Here is the new post page</h1>
-                <PlaylistSelectForm />
-            </div>
-            
-        );
-    }
+    );
 }
 
 export default PostNew;
